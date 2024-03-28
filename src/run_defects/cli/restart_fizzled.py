a163a610bc992259b177b74546794a5107bfa4fc
jmmshn <jmmshn@gmail.com>
2024-03-28 21:14:52 +0000

    restart

---
+++ src/run_defects/cli/restart_fizzled.py
@@ -0,0 +1,93 @@
+"""Restart fizzled FireWorks with updated INCAR settings."""
+
+from __future__ import annotations
+
+import json
+import logging
+
+import click
+from fireworks import LaunchPad
+
+from run_defects.fw_mod import (
+    _get_fw_id_by_state,
+    _get_incar_diff,
+    _get_last_launch_dir,
+    _read_incar,
+    get_charged_structure,
+    update_incar_settings,
+    update_struct,
+)
+
+LPAD = LaunchPad.auto_load()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+# # %%
+# ready_ids = _get_fw_id_by_state(state="READY")
+# for fw_id in ready_ids:
+#     last_launch_dir = _get_last_launch_dir(LPAD.get_fw_dict_by_id(fw_id))
+#     update_incar_settings(fw_id, {"LREAL": "Auto"}, check_fizzled=False, dry_run=True)
+
+# #%%
+# fw_id = _get_fizzled()[0]
+# fw_dict = LPAD.get_fw_dict_by_id(fw_id)
+# last_launch_dir = _get_last_launch_dir(fw_dict)
+# contcar = _read_contcar(last_launch_dir)
+# incar_orig, incar = _read_incar(last_launch_dir)
+# incar_update_dict = _get_incar_diff(incar_orig, incar)
+
+# # get the incar values that are different
+# incar_diff = _get_incar_diff(incar_orig, incar)
+# if swap_ibrion:
+#     _swap_val = {1: 2, 2: 1}
+#     incar_diff["IBRION"] = _swap_val[incar["IBRION"]]
+
+# update_fizzled_incar_settings(fw_id, incar_diff, dry_run=False)
+# cstruct = get_charged_structure(fw_id)
+# update_struct(fw_id, cstruct)
+
+
+# %%
+
+
+# %%
+@click.command()
+@click.option("-q", "--query", default={})
+@click.option("-d", "--dry-run", is_flag=True, show_default=True, default=False)
+@click.option("-i", "--fw-id")
+@click.option(
+    "-b",
+    "--ibrion-swap",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Swap IBRION value.",
+)
+def main(
+    query: str, dry_run: bool, fw_id: int | None = None, ibrion_swap: bool = False
+) -> None:
+    """Run the CLI."""
+    q = json.loads(query)
+    fizzled = [int(fw_id)] if fw_id is not None else _get_fw_id_by_state(q)
+    for fw_id in fizzled:
+        structure = get_charged_structure(fw_id)
+        last_launch_dir = _get_last_launch_dir(LPAD.get_fw_dict_by_id(fw_id))
+        incar_orig, incar = _read_incar(last_launch_dir)
+        incar_update_dict = _get_incar_diff(incar_orig, incar)
+        logger.info(f"Launch Dir: {last_launch_dir}")
+        logger.info(f"INCAR Update: {incar_update_dict}")
+        logger.info(f"Parsed Structure: {structure.formula}")
+
+        if not dry_run:
+            update_struct(fw_id, structure)
+            if ibrion_swap:
+                _swap_val = {1: 2, 2: 1, 3: 1}
+                incar_update_dict["IBRION"] = _swap_val[incar["IBRION"]]
+            update_incar_settings(fw_id, incar_update_dict, dry_run=False)
+            LPAD.rerun_fw(fw_id)
+
+
+# %%
+if __name__ == "__main__":
+    main()
---
+++ src/run_defects/fw_mod.py
@@ -0,0 +1,235 @@
+"""Modify atomate2 fireworks in database."""
+
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from fireworks import LaunchPad
+from monty.json import MontyDecoder, jsanitize
+from pymatgen.io.vasp.inputs import Incar, Poscar
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+    from pymatgen.core import Structure
+
+logger = logging.getLogger(__name__)
+
+LPAD_AUTO_LOAD = LaunchPad.auto_load()
+
+
+def _get_fw_id_by_state(
+    query: dict | None = None, state: str = "FIZZLED", lpad: LaunchPad = None
+) -> list:
+    """Find the fizzed relaxation jobs.
+
+    The atomate2 created fireworks for long running relaxation jobs should be simple.
+    Basically `spec._tasks.0` should be the relaxation job.
+
+    Args:
+        query: query to use.  The basic query is already included.
+        state: state to search for, default FIZZLED
+        lpad: LaunchPad object
+
+
+    Returns:
+        list: list of fizzled fireworks ids
+    """
+    if lpad is None:
+        lpad = LPAD_AUTO_LOAD
+    fw_query = {
+        "spec._tasks.0.job.function.@callable": "BaseVaspMaker.make",
+        "state": state,
+        "$or": [
+            {"spec._tasks.0.job.function_args.0.@class": "Structure"},
+            {"spec._tasks.0.job.function_args.0.@class": "OutputReference"},
+            {"spec._tasks.0.job.function_args.0.@class": "Poscar"},
+        ],
+    }
+    fw_query.update(query or {})
+    return lpad.get_fw_ids(query=fw_query)
+
+
+def _get_last_launch_dir(fw_dict: dict) -> str:
+    """Get the last launch directory for a fizzled job.
+
+    Args:
+        fw_dict: fireworks id
+
+    Returns:
+        str: last launch directory
+    """
+    launches = fw_dict.get("launches")
+    if launches is None:
+        launches = fw_dict.get("archived_launches")
+    if launches is None or len(launches) == 0:
+        raise ValueError(f"Could not find launches for {fw_dict['fw_id']}")
+    return launches[-1]["launch_dir"]
+
+
+def _read_contcar(dir_name: str) -> Poscar:
+    """Read the CONTCAR currently in the directory.
+
+    Args:
+            dir_name: directory to read CONTCAR from
+
+    Returns:
+            Structure: structure from CONTCAR
+    """
+    # check for CONTCAR or CONTCAR.gz
+    sub_dir = Path(dir_name)
+    try:
+        contcar_file = next(sub_dir.glob("CONTCAR*"))
+        contcar = Poscar.from_file(contcar_file)
+    except Exception as e:
+        raise RuntimeError(f"Could not read CONTCAR from {dir_name}") from e
+    return contcar
+
+
+def _read_incar(dir_name: str) -> tuple[Incar, Incar]:
+    """Read the INCAR currently in the directory.
+
+    Args:
+            dir_name: directory to read INCAR from
+
+    Returns:
+            incar_orig: original INCAR
+            incar: current INCAR
+    """
+    try:
+        incar_orig = Incar.from_file(Path(dir_name) / "INCAR.orig")
+        incar = Incar.from_file(Path(dir_name) / "INCAR")
+    except Exception as e:
+        raise RuntimeError(f"Could not read INCAR from {dir_name}") from e
+        return None, None
+    return incar_orig, incar
+
+
+def _recursive_update(
+    orig: dict | list, update_dict: dict, path: str = "_tasks"
+) -> Generator:
+    """Update a dictionary recursively.
+
+    Check if orig[key] is in update_dict, if it is then update it.
+    Else, call _recursive_update(orig[key], update_dict)
+    """
+    if isinstance(orig, dict):
+        for key, v in orig.items():
+            if key in update_dict:
+                if not isinstance(v, dict):
+                    raise RuntimeError("Can only update dict.")
+                for k_, v_ in update_dict[key].items():
+                    yield f"{path}.{key}.{k_}", v_
+            new_path = f"{path}.{key}"
+            if "archived_launches" not in path:
+                yield from _recursive_update(orig[key], update_dict, path=new_path)
+
+    elif isinstance(orig, list):
+        for i, v in enumerate(orig):
+            new_path = f"{path}.{i}"
+            if "archived_launches" not in path:
+                yield from _recursive_update(v, update_dict, path=new_path)
+
+
+def _update_fizzled_firework(
+    fw_id: int,
+    update_dict: dict,
+    dry_run: bool = True,
+    check_fizzled: bool = True,
+    lpad: LaunchPad = None,
+) -> None:
+    """Update a fizzled firework.
+
+    Args:
+        fw_id: fireworks id.
+        update_dict: dictionary of updates.
+        dry_run: dry run, default True, only print update dict.
+        check_fizzled: check if the firework is fizzled, default True.
+        lpad: LaunchPad object, default will use auto_load.
+    """
+    # make sure the firework is fizzled
+    if lpad is None:
+        lpad = LPAD_AUTO_LOAD
+    fw_dict = lpad.get_fw_dict_by_id(fw_id)
+    if check_fizzled and fw_dict["state"] != "FIZZLED":
+        raise RuntimeError("Firework must be fizzled.")
+
+    updates_dict = dict(_recursive_update(fw_dict["spec"]["_tasks"], update_dict))
+    if dry_run:
+        logger.debug(f"Would update {fw_id} with {updates_dict}")
+    else:
+        lpad.update_spec([fw_id], updates_dict)
+
+
+def update_incar_settings(
+    fw_id: int,
+    user_incar_setting: dict,
+    check_fizzled: bool = True,
+    dry_run: bool = True,
+) -> None:
+    """Update a fizzled firework.
+
+    Args:
+        fw_id: fireworks id.
+        user_incar_setting: dictionary of updates. Ex: {"EDIFFG": -0.005}
+        check_fizzled: check if the firework is fizzled, default True.
+        dry_run: dry run, default True, only print update dict.
+    """
+    _update_fizzled_firework(
+        fw_id=fw_id,
+        update_dict={"user_incar_settings": user_incar_setting},
+        dry_run=dry_run,
+        check_fizzled=check_fizzled,
+    )
+
+
+def update_struct(fw_id: int, structure: Structure, lpad: LaunchPad = None) -> None:
+    """Update the spec structure for a fizzled job.
+
+    Args:
+        fw_id: fireworks id.
+        structure: structure to update to.
+        lpad: LaunchPad object, default will use auto_load.
+    """
+    if lpad is None:
+        lpad = LPAD_AUTO_LOAD
+    update_dict = {
+        "_tasks.0.job.function_args.0": jsanitize(structure.as_dict()),
+    }
+    lpad.update_spec([fw_id], update_dict)
+
+
+def _get_incar_diff(incar_old: Incar, incar_new: Incar) -> dict:
+    """Get the INCAR differences between two INCARs."""
+    incar_diff = {}
+    for k, v in incar_new.items():
+        if incar_old.get(k, None) != v:
+            incar_diff[k] = v
+    return incar_diff
+
+
+def get_charged_structure(fw_id: int, lpad: LaunchPad = None) -> Structure:
+    """Read the CONTCAR if available and decorate with the charge state.
+
+    Args:
+        fw_id: fireworks id
+        lpad: LaunchPad object, default will use auto_load.
+    """
+    if lpad is None:
+        lpad = LPAD_AUTO_LOAD
+    fw_dict = lpad.get_fw_dict_by_id(fw_id)
+    dir_name = _get_last_launch_dir(fw_dict)
+    if dir_name is None:
+        raise RuntimeError(f"Could not find a launch directory for {fw_id}")
+    logger.debug(f"Get the charged structure for {fw_id} from {dir_name}")
+    contcar = _read_contcar(dir_name)
+    structure: Structure = contcar.structure
+
+    old_struct = fw_dict["spec"]["_tasks"][0]["job"]["function_args"][0]
+    old_struct = MontyDecoder().process_decoded(
+        fw_dict["spec"]["_tasks"][0]["job"]["function_args"][0]
+    )
+    structure._charge = old_struct._charge  # : SLF001
+    return structure
