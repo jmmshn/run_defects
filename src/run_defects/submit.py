8b37fbc00337503f1d09a5577ce70ff27ce34e17
@jmmshn <jmmshn@gmail.com>
2024-03-28 14:24:12 -0700

    submit

--- src/run_defects/submit.py
+++ src/run_defects/submit.py
@@ -1,25 +1,24 @@
+"""Submit the workflows to firework."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import jobflow
 from atomate2.vasp.flows.defect import FormationEnergyMaker
 from atomate2.vasp.flows.mp import MPGGADoubleRelaxMaker, MPGGAStaticMaker
 from atomate2.vasp.jobs.mp import MPGGARelaxMaker
-from atomate2.vasp.sets.mp import MPGGARelaxSetGenerator
 from atomate2.vasp.powerups import (
-    add_metadata_to_flow,
     update_user_incar_settings,
     update_user_kpoints_settings,
 )
 from atomate2.vasp.sets.defect import SPECIAL_KPOINT
-from js_mp.query import get_doc_from_fwid, get_docs
-from js_mp.submit import submit_flow
-from monty.json import MontyDecoder
-from pymatgen.analysis.defects.generators import VacancyGenerator
-from pymatgen.analysis.defects.supercells import get_sc_fromstruct
-from pymatgen.analysis.structure_matcher import ElementComparator, StructureMatcher
-from pymatgen.core import Structure
-from pymatgen.ext.matproj import MPRester
-from fireworks import LaunchPad
+from atomate2.vasp.sets.mp import MPGGARelaxSetGenerator
 from jobflow.managers.fireworks import flow_to_workflow
-from pymatgen.analysis.defects.supercells import get_sc_fromstruct
+
+if TYPE_CHECKING:
+    from fireworks import LaunchPad
+    from pymatgen.core import Structure
 
 INCAR_UPDATES = {
     "EDIFF": 1e-5,
@@ -53,7 +52,13 @@ BULK_STATIC_SC = update_user_kpoints_settings(
     BULK_STATIC_SC, kpoints_updates=SPECIAL_KPOINT
 )
 
-DEFECT_RELAX_SC = update_user_incar_settings(MPGGARelaxMaker(input_set_generator=MPGGARelaxSetGenerator(use_structure_charge=True), task_document_kwargs={"store_volumetric_data": ["locpot",]},), incar_updates=INCAR_UPDATES | {"ISIF": 2, "LVHAR": True})
+DEFECT_RELAX_SC = update_user_incar_settings(
+    MPGGARelaxMaker(
+        input_set_generator=MPGGARelaxSetGenerator(use_structure_charge=True),
+        task_document_kwargs={"store_volumetric_data": ["locpot"]},
+    ),
+    incar_updates=INCAR_UPDATES | {"ISIF": 2, "LVHAR": True},
+)
 DEFECT_RELAX_SC = update_user_kpoints_settings(
     DEFECT_RELAX_SC, kpoints_updates=SPECIAL_KPOINT
 )
@@ -75,7 +80,7 @@ F_MAKER_SC = FormationEnergyMaker(
 )
 
 
-def add_tag(flow, tag):
+def add_tag(flow: jobflow.Flow, tag: str) -> None:
     """Update the metadata of the flow to include a tag."""
     for job in flow.jobs:
         if isinstance(job, jobflow.Flow):
@@ -87,11 +92,13 @@ def add_tag(flow, tag):
             job.metadata.update({"tags": list(tags)})
 
 
-def submit_flow(flow: jobflow.Flow, tag, lpad):
+def submit_flow(flow: jobflow.Flow, tag: str, lpad: LaunchPad) -> None:
     """Submit the flow to firework.
 
     Args:
         flow (Flow): flow to submit to firework
+        tag (str): tag to add to the flow
+        lpad (LaunchPad): firework launchpad
     """
     add_tag(flow, tag)
     lpad.add_wf(flow_to_workflow(flow))
@@ -102,16 +109,19 @@ def get_bulk_flow(
     relax_maker: jobflow.Maker | None = None,
     static_maker: jobflow.Maker | None = None,
     remove_symmetry: bool = True,
-):
+) -> jobflow.Flow:
     """Get the bulk workflow for a structure.
 
     (double relax, static)
 
     Args:
         structure (Structure): structure to compute
-        relax_maker (jobflow.Maker, optional): maker for the relax job. Defaults to None.
-        static_maker (jobflow.Maker, optional): maker for the static job. Defaults to None.
-        remove_symmetry (bool, optional): remove symmetry from the structure. Defaults to True.
+        relax_maker (jobflow.Maker, optional): maker for the relax job.
+            Defaults to None.
+        static_maker (jobflow.Maker, optional): maker for the static job.
+            Defaults to None.
+        remove_symmetry (bool, optional): remove symmetry from the structure.
+            Defaults to True.
 
     Returns:
         jobflow.Flow: bulk flow
